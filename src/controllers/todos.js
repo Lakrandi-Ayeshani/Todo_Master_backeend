6cb533b09e548b93ceeb06be3e0d5f70c35462f7
Lakrandi-Ayeshani <Lakrandiayeshani1204@gmail.com>
2023-07-10 23:16:39 +0530

    fetchTodo, createTodo, updataTodo, deleteTodo end points created

--- src/controllers/todos.js
+++ src/controllers/todos.js
@@ -16,7 +16,15 @@ export const getTodos = (async (req, res) => {
 // fetch a todo
 
 export const getTodo = (async (req, res) => {
-  "hello"
+    const fetchTodoId = req.params.todoID;
+    const response = await todoModel.findById(fetchTodoId);
+
+    try {
+        res.json(response)
+    }
+    catch (err) {
+        console.error(err);
+    }
 });
 
 // create a todo
@@ -24,23 +32,38 @@ export const getTodo = (async (req, res) => {
 export const createTodo = (async(req, res) => {
     const todo = new todoModel(req.body);
     try {
-        const response = await todo.save();
-        console.log(todo);
-
+        const response = await todoModel.create(todo);
+        res.json(response);
     }
     catch(err) {
-        console.error(err)
+        console.error(err);
     }
 });
 
 // Update a todo
 
 export const updateTodo = (async (req, res) => {
-"helooo"
+    const updateTodoId = req.params.todoID;
+    const newTodo = req.body;
+    try {
+        const response = await todoModel.findByIdAndUpdate(updateTodoId, newTodo);
+        res.json(response);
+    }
+    catch (err) {
+        console.error(err);
+    }
+
 });
 
 // delete a todo
 
 export const deleteTodo = (async (req, res) => {
-"helooo"
+    const deleteId = req.params.todoID;
+    try {
+        const response = await todoModel.findByIdAndDelete(deleteId);
+        res.json(response);
+    }
+    catch (err) {
+        console.error(err);
+    }
 });
